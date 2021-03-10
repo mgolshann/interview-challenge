51db68b370ef93ae000497e56a8f5e7e65caf02e
mgolshann <m_golshan85@yahoo.com>
2021-03-09 22:10:39 -0800

    create covid 19 tracker components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "IR"]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <h1>React Project</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            varient="outlined"
+            value="abc"
+          >
+            {
+              countries.map((country) => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
